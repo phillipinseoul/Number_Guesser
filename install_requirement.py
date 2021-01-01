287154b162e6c8666ebf87c9e9695a55415c17c9
phillip9807@gmail.com <phillip9807@gmail.com>
2021-01-01 21:43:27 +0900

    install func

--- install_requirement.py
+++ install_requirement.py
@@ -5,3 +5,11 @@ import os
 import importlib
 import contextlib
 
+def install(package):
+    '''
+    installs a package using pip
+
+    :param package: string
+    '''
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
